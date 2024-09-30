40759a15118bf3e392bd67b3a3239c91e525f8a8
Jeronimo Reguera <Jero2760@users.noreply.github.com>
2024-09-30 12:46:18 +0000

    modified:   requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+df = pd.DataFrame(data={'Exam':['Exam 1','Exam 2','Exam 3'],
+'Jessica':[77,76,87],'John':[56,97,95],'Alex':[87,82,93]}
+                  )
+fig = go.Figure(data=[
+    go.Line(name='Jessica', x=df['Exam'], y=df['Jessica']),
+    go.Line(name='John', x=df['Exam'], y=df['John']),
+    go.Line(name='Alex', x=df['Exam'], y=df['Alex'])
+])
+
+fig.update_layout(
+    xaxis_title='Exam',
+    yaxis_title='Score',
+    legend_title='Name',
 )
+
+st.plotly_chart(fig)
